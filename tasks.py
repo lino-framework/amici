b8978448d0ee4a4960e06b53a15131ff77554d41
Luc Saffre <luc.saffre@gmail.com>
2017-06-14 05:37:48 +0300

    http://luc.lino-framework.org/blog/2017/0614.html

--- tasks.py
+++ tasks.py
@@ -6,4 +6,6 @@ ns.setup_from_tasks(
     blogref_url='http://luc.lino-framework.org',
     revision_control_system='git',
     locale_dir='lino_amici/lib/amici/locale',
+    demo_projects=[
+        'lino_amici.projects.herman.settings.demo'],
 )
